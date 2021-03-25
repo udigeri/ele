fab2f420c1653cdce9d8b2f63f9e83b85dd696e2
Udi <udigeri@gmail.com>
2021-03-25 21:33:27 +0100

    minor changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- settings for logging into server
+
+## [0.1.0](https://github.com/udigeri/ele/tree/v0.1.0) (2021-03-25)
+
 ### Added 
 
 - Added base structure of app
--- ele/app.py
+++ ele/app.py
@@ -3,14 +3,14 @@ from logger import EleLogger
 from elevation import Elevation
 
 class App(object):
-    APP_INITAL_MSG = "KST elevation profiler"
+    APP_INITIAL_MSG = "KST elevation profiler"
 
     def __init__(self, version, params):
         self.version = version
-        print("{} {}".format(self.APP_INITAL_MSG, self.version))
+        print("{} {}".format(self.APP_INITIAL_MSG, self.version))
         self.config = Config(params = params)
         self.logger = EleLogger(self.config, version)
-        self.logger.info(self.APP_INITAL_MSG)
+        self.logger.info(self.APP_INITIAL_MSG)
         # self.logger.debug(self.APP_INITAL_MSG)
         # self.logger.warn(self.APP_INITAL_MSG)
         # self.logger.error(self.APP_INITAL_MSG)
